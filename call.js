1975472db8784f0fd97157266b1bb71a411d7f94
habibullayev7558883@gmail.com <habibullayev7558883@gmail.com>
2026-03-04 23:10:32 +0500

    add call page

---
+++ call.js
@@ -0,0 +1,46 @@
+const chatToken = "8787864164:AAEk5nhOX4WNnCfLAeYGfTJ4Zt0MrxhWCug";
+const chatID = "7431732682";
+
+let elName = document.querySelector("#ism")
+let elLastName = document.querySelector("#familya")
+let elNum = document.querySelector("#telefon")
+
+let elForm = document.querySelector("#myForm")
+
+
+elForm.addEventListener("submit",(e)=>{
+  e.preventDefault();
+
+
+  const  foydalanuvchiMalumotlari = `
+📩 Yangi foydalanuvchi:
+
+👤 Ism: ${elName.value}
+👤 Familiya: ${elLastName.value}
+🎂 Yosh: ${elNum.value}
+
+  `;
+
+  
+  fetch(`https://api.telegram.org/bot${chatToken}/sendMessage`, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },  
+  body: JSON.stringify({
+    chat_id: chatID,
+    text: foydalanuvchiMalumotlari  
+  })
+
+  
+
+  })
+  .then(res=>res.json())
+  .then(data=>{
+    alert("yuborildi:", data);
+  })          
+  .catch(error=>{
+    alert("xatolik:",error);
+  })
+
+})
